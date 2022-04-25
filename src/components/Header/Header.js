58164627c58c10fa735c0c8252aa83b79f060973
Jaroslaw <jaroslaw.chaikin@gmail.com>
2022-04-25 22:58:02 +0300

    theme icons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,21 @@
 import classes from './Header.module.scss'
+import { changeTheme } from '../../store/reducers/changeThemeReducer';
+import { useDispatch, useSelector } from 'react-redux';
+import moon from '../../assets/images/moon.svg'
+import sun from '../../assets/images/sun.svg'
+
 const Header = () => {
+    const dispatch = useDispatch()
+    const theme = useSelector(state => state.theme)
+
+    const changeThm = () => dispatch(changeTheme())
+
     return (
       <div className={classes.header}>
             <h1 className={classes.title}>TODO</h1>
+            <div className={classes.themeImage} onClick={changeThm}>
+                <img src={theme === 'black' ? sun : moon} alt="theme"/>
+            </div>
       </div>
     );
 };
