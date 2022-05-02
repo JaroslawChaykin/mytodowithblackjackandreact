e40983a58d5bad0f858281672cf30a00deb8cfa6
Jaroslaw <jaroslaw.chaikin@gmail.com>
2022-05-02 16:25:33 +0300

    Absolute Imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import TodoList from './components/todoList/TodoList';
-import BgImage from './components/bg-image/BgImage';
-import TodoCreatedForm from './components/todoCreatedForm/TodoCreatedForm';
-import Header from './components/Header/Header';
-import './App.css';
-import FooterFilter from './components/footerFilter/FooterFilter';
+import TodoList from 'components/todoList/TodoList';
+import BgImage from 'components/bg-image/BgImage';
+import TodoCreatedForm from 'components/todoCreatedForm/TodoCreatedForm';
+import Header from 'components/Header/Header';
+import 'App.css';
+import FooterFilter from 'components/footerFilter/FooterFilter';
 
 function App() {
 
